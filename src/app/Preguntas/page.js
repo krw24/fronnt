7b511c71318bd47934260381a286ff07b333fcde
JuanesEsp1 <juanes.espinosa01@gmail.com>
2024-11-08 02:49:03 -0500

    .

--- src/app/Preguntas/page.js
+++ src/app/Preguntas/page.js
@@ -1,96 +1,122 @@
 "use client";
 
-import { useEffect } from "react"; 
+import { useEffect, useState } from "react"; 
 import { FiSearch, FiLogOut } from "react-icons/fi";
 import useQuestions from "./useQuestions";
 import useChatClient from "./useChatClient";
 import { Chat } from "../../components/chat/Chat.js";
 import { Toaster } from "react-hot-toast";
+import { TiMessages } from "react-icons/ti";
+import { LuMessageSquarePlus } from "react-icons/lu";
 
 const Preguntas = () => {
   const { userLoged, chatIsOpen, setChatIsOpen, onSubmit, handleSubmit, register } = useQuestions();
+  const [view, setView] = useState(0);
   const chatLogic = useChatClient(userLoged, chatIsOpen);
 
   return (
-    <div className="w-full h-screen flex flex-col relative">
-      <div className="w-full h-24 flex items-center justify-between px-8 font-semibold bg-indigo-300">
-        <div className="w-full text-6xl  font-semibold bg-indigo-200">
-          Soporte
+    <div className="w-full h-screen flex flex-row relative">
+      <div className="w-24 h-screen flex flex-col  bg-indigo-400">
+        <div className="w-full h-24 flex justify-center items-center bg-indigo-300"></div>
+        <div onClick={() => setView(0)} className="w-full h-20 flex justify-center items-center border-b-[1px] border-indigo-100 hover:opacity-60 cursor-pointer">
+          <LuMessageSquarePlus className="text-5xl text-indigo-100" />
         </div>
-        <div className=" flex items-center flex-row gap-2">
-          <div className="w-72 flex items-center gap-2 justify-center">
-            <div className="w-12 h-12 bg-white border-[1px] border-indigo-500 rounded-full flex items-center justify-center">
-              <span className="text-3xl font-bold text-indigo-500">
-                {userLoged?.name?.charAt(0).toUpperCase()}
-              </span>
-            </div>
-            <div className="flex flex-row gap-2">
-              <div className="text-lg font-semibold">{userLoged.name}</div>
-              <div
-                onClick={() => {
-                  localStorage.removeItem("token");
-                  window.location.href = "/";
-                }}
-                className="cursor-pointer translate-x-1 hover:translate-x-2 transition-all duration-300"
-              >
-                <FiLogOut className="text-3xl pl-2" />
+        <div onClick={() => setView(1)} className="w-full h-20 flex justify-center items-center border-b-[1px] border-indigo-100 hover:opacity-60 cursor-pointer">
+          <TiMessages className="text-5xl text-indigo-100" />
+        </div>
+        <div className="w-full h-24 flex justify-center items-center "></div>
+        <div className="w-full h-24 flex justify-center items-center "></div>
+      </div>
+      <div className="w-full h-screen flex flex-col">
+        <div className="w-full h-24 flex items-center justify-between px-8 font-semibold bg-indigo-300">
+          <div className="w-full text-6xl  font-semibold ">
+            Soporte
+          </div>
+          <div className=" flex items-center flex-row gap-2">
+            <div className="w-72 flex items-center gap-2 justify-center">
+              <div className="w-12 h-12 bg-white border-[1px] border-indigo-500 rounded-full flex items-center justify-center">
+                <span className="text-3xl font-bold text-indigo-500">
+                  {userLoged?.name?.charAt(0).toUpperCase()}
+                </span>
+              </div>
+              <div className="flex flex-row gap-2">
+                <div className="text-lg font-semibold">{userLoged.name}</div>
+                <div
+                  onClick={() => {
+                    localStorage.removeItem("token");
+                    window.location.href = "/";
+                  }}
+                  className="cursor-pointer translate-x-1 hover:translate-x-2 transition-all duration-300"
+                >
+                  <FiLogOut className="text-3xl pl-2" />
+                </div>
               </div>
             </div>
           </div>
         </div>
         <div className="w-full h-[calc(100vh-88px)] flex items-center justify-center">
-          <div className="w-[500px] h-[400px] p-5 bg-white rounded-xl shadow-xl ">
-            <div className="w-full flex flex-col gap-2"> 
-              <div className="flex justify-center font-semibold text-3xl">Generador de PQRs</div>
-              <div className="flex justify-center font-light text-md text-center">
-                En este apartado podrás crear una petición para que un administrador pueda responderte lo antes posible.
+          { view === 0 &&
+            <div className="w-[500px] h-[400px] p-5 bg-white rounded-xl shadow-xl ">
+              <div className="w-full flex flex-col gap-2"> 
+                <div className="flex justify-center font-semibold text-3xl">Generador de PQRs</div>
+                <div className="flex justify-center font-light text-md text-center">
+                  En este apartado podrás crear una petición para que un administrador pueda responderte lo antes posible.
+                </div>
               </div>
+              <form className="w-full flex flex-col gap-4 h-full p-4 pt-6" onSubmit={handleSubmit(onSubmit)}>
+                <div className="relative h-11">
+                  <select
+                    id="testMode"
+                    className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-[2.5px] border-gray-300  focus:outline-none focus:ring-0 focus:border-[#a5aefc] focus:border-[2.5px] peer"
+                    defaultValue={""}
+                    {...register("type")}
+                  >
+                    <option disabled value={""}>
+                      Seleccione una opción
+                    </option>
+                    <option value="peticion">Peticion</option>
+                    <option value="queja">Queja</option>
+                    <option value="reclamo">Reclamo</option>
+                  </select>
+                  <label
+                    htmlFor="testMode"
+                    className="absolute text-md  dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white  px-2 peer-focus:px-2 peer-focus:text-[#a5aefc]  peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
+                  >
+                    Tipo de peticion
+                  </label>
+                </div>
+                <div className="relative ">
+                  <textarea
+                    id="description"
+                    rows={4}
+                    className="min-h-[111px] max-h-[111px] block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-[2.5px] border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-[#a5aefc] focus:border-[2.5px] peer"
+                    placeholder=" "
+                    {...register("description")}
+                  />
+                  <label
+                    htmlFor="description"
+                    className="absolute text-md text-slate-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white  px-2 peer-focus:px-2 peer-focus:text-[#a5aefc]  peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-[20%] peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-2"
+                  >
+                    Descripcion
+                  </label>
+                </div>
+                <div className="w-full flex justify-end">
+                  <button className="bg-indigo-500 text-white px-4 py-2 rounded-md hover:bg-indigo-600">
+                    Enviar
+                  </button>
+                </div>
+              </form>
             </div>
-            <form className="w-full flex flex-col gap-4 h-full p-4 pt-6" onSubmit={handleSubmit(onSubmit)}>
-              <div className="relative h-11">
-                <select
-                  id="testMode"
-                  className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-[2.5px] border-gray-300  focus:outline-none focus:ring-0 focus:border-[#a5aefc] focus:border-[2.5px] peer"
-                  defaultValue={""}
-                  {...register("type")}
-                >
-                  <option disabled value={""}>
-                    Seleccione una opción
-                  </option>
-                  <option value="peticion">Peticion</option>
-                  <option value="queja">Queja</option>
-                  <option value="reclamo">Reclamo</option>
-                </select>
-                <label
-                  htmlFor="testMode"
-                  className="absolute text-md  dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white  px-2 peer-focus:px-2 peer-focus:text-[#a5aefc]  peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
-                >
-                  Tipo de peticion
-                </label>
-              </div>
-              <div className="relative ">
-                <textarea
-                  id="description"
-                  rows={4}
-                  className="min-h-[111px] max-h-[111px] block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-[2.5px] border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-[#a5aefc] focus:border-[2.5px] peer"
-                  placeholder=" "
-                  {...register("description")}
-                />
-                <label
-                  htmlFor="description"
-                  className="absolute text-md text-slate-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white  px-2 peer-focus:px-2 peer-focus:text-[#a5aefc]  peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-[20%] peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-2"
-                >
-                  Descripcion
-                </label>
-              </div>
-              <div className="w-full flex justify-end">
-                <button className="bg-indigo-500 text-white px-4 py-2 rounded-md hover:bg-indigo-600">
-                  Enviar
-                </button>
-              </div>
-            </form>
-          </div>
+          }
+          {
+            view === 1 &&
+            <div className="w-[500px] h-[400px] p-5 bg-black rounded-xl shadow-xl ">
+                
+            </div>
+          }
+            
         </div>
+
       </div>
       {chatIsOpen ? (
         <Chat
